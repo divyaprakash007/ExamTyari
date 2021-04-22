94068dba70758fb7b29463ac6b7d9be750ca9b55
DIVYA PRAKASH <Sunaina@123>
2021-04-22 14:47:31 +0530

    NCERT PagerAdapter set, Tested Okay.

--- app/src/main/java/dp/ibps/generalawareness/Service/FCMMessageService.java
+++ app/src/main/java/dp/ibps/generalawareness/Service/FCMMessageService.java
@@ -1,6 +1,8 @@
 package dp.ibps.generalawareness.Service;
 
 import android.app.Notification;
+import android.app.NotificationManager;
+import android.graphics.Color;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.NotificationCompat;
@@ -8,17 +10,30 @@ import androidx.core.app.NotificationCompat;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 
+import dp.ibps.generalawareness.R;
+
 public class FCMMessageService extends FirebaseMessagingService {
 
     @Override
     public void onMessageReceived(@NonNull RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
-//        if (remoteMessage.getNoKtification() != null){
-//             String title = remoteMessage.getNotification().getTitle();
-//             String body = remoteMessage.getNotification().getBody();
-//
-////            Notification notification = new NotificationCompat().Builder(this, )
-//        }
+        if(remoteMessage.getNotification() != null){
+            String title = remoteMessage.getNotification().getTitle();
+            String body = remoteMessage.getNotification().getBody();
+
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                Notification notification = new Notification.Builder(this,
+                        "FCM_CHANNEL_ID").setSmallIcon(R.drawable.ic_baseline_notifications_24)
+                        .setContentTitle(title)
+                        .setContentText(body)
+                        .setColor(Color.BLUE)
+                        .build();
+
+                NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                manager.notify(1002,notification);
+
+            }
+        }
     }
 
     @Override
